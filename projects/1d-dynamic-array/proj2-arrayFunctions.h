c92a7ead6b14ea16566947fe989b7cfcdacec049
firesquid6 <jdeiss06@gmail.com>
2025-01-28 16:28:39 -0600

    add the test bench

--- projects/1d-dynamic-array/proj2-arrayFunctions.h
+++ projects/1d-dynamic-array/proj2-arrayFunctions.h
@@ -1,7 +1,7 @@
 #ifndef PROJ2_ARRAYFUNCTIONS_H
 #define PROJ2_ARRAYFUNCTIONS_H
 
-void bubbleSort(int *array, int size);
+void bubbleSort(int *array, int size); 
 int largestValue(int *array, int size); // used for largest in single array
 int smallestValue(int *array, int size); // used for smallest in single array
 double averageValue(int *array, int size);
