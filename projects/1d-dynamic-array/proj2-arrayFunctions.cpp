8ec62f162051eaf5cdb17aabf46948111d3ded77
firesquid6 <jdeiss06@gmail.com>
2025-01-28 16:48:13 -0600

    add tested functions

--- projects/1d-dynamic-array/proj2-arrayFunctions.cpp
+++ projects/1d-dynamic-array/proj2-arrayFunctions.cpp
@@ -1,16 +1,52 @@
 #include "proj2-arrayFunctions.h"
+#include <iostream>
 
+using namespace std;
 
 void bubbleSort(int *array, int size) {
+  bool swapped;
+  for (int i = 0; i < size - 1; i++) {
+    swapped = false;
 
+    for (int j = 0; j < size - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array[j], array[j + 1]);
+        swapped = true;
+      }
+    }
+
+    if (!swapped) {
+      break;
+    }
+  }
 }
 
-int largestValue(int *array, int size) {
-  return 0;
+int largestValue(int *array, int size) { 
+  if (size == 0) {
+    return -1;
+  }
+  int max = array[0];
+  for (int i = 1; i < size; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  return max;
 }
 
-int smallestValue(int *array, int size) {
-  return 0;
+int smallestValue(int *array, int size) { 
+  if (size == 0) {
+    return -1;
+  }
+  int min = array[0];
+  for (int i = 1; i < size; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+
+  return min;
 }
 
 double averageValue(int *array, int size) {
@@ -21,10 +57,9 @@ double averageValue(int *array, int size) {
   }
 
   return total / size;
-
 }
 
-int medianValue(int *array, int size) {
-
+int medianValue(int *array, int size) { 
+  int index = (size - 1) / 2;
+  return array[index];
 }
-
--- projects/1d-dynamic-array/test.sh
+++ projects/1d-dynamic-array/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "COMPILING..."
-g++ ./proj2-arrayFunctions.cpp ./proj2-testmain.cpp -Wall -g -o main.o
+g++ ./proj2-arrayFunctions.cpp ./proj2-testmain.cpp -Wall -g -o test.o
 echo "TESTING..."
 ./test.o
