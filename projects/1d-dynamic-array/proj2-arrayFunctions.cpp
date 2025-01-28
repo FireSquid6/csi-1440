995e92e6c3b781d35ee7adac341c0f6a9fd05e4a
firesquid6 <jdeiss06@gmail.com>
2025-01-28 16:21:43 -0600

    add text

---
+++ projects/1d-dynamic-array/proj2-arrayFunctions.cpp
@@ -0,0 +1,30 @@
+#include "proj2-arrayFunctions.h"
+
+
+void bubbleSort(int *array, int size) {
+
+}
+
+int largestValue(int *array, int size) {
+  return 0;
+}
+
+int smallestValue(int *array, int size) {
+  return 0;
+}
+
+double averageValue(int *array, int size) {
+  double total = 0;
+
+  for (int i = 0; i < size; i++) {
+    total += array[i];
+  }
+
+  return total / size;
+
+}
+
+int medianValue(int *array, int size) {
+
+}
+
