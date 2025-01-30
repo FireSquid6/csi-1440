e81d546d6300e90b3a7a0fafae611408f4a2ec22
firesquid6 <jdeiss06@gmail.com>
2025-01-30 17:47:54 -0600

    add more answers for lab 1

--- labs/lab-1/lab1-answersToQuestions.h
+++ labs/lab-1/lab1-answersToQuestions.h
@@ -2,6 +2,23 @@
  * Name: Jonathan Deiss
  * CSI 1440 Section 1
  * Lab 1
+ *
+ * "what are the differences between the original value and the incremented value?"
+ * The incremented value is one "size" of whatever it was pointing at away from wherever it originally pointed. The pointer
+ * is at a new address
+ *
+ * "is this what you expected?"
+ * I expected intPtr to point to ch, chPtr to point to dbl, and dblPtr to cause a segfault if dereferenced.
+ *
+ * "Provide an explanation of the results"
+ * 
+ * pointers now point to:
+ * intPtr -> dbl
+ * chPtr -> x
+ * dblPtr -> somewhere else entirely
+ *
+ * what surprised me more was that when dereferenced, dblPtr did not cause a segfault. I would have thought that it was touching memory
+ * that it wasn't supposed to, which is bad.
  * 
  * "Explain the difference between *, &, and just the variables name" 
  *  *x - x is a variable that stores a memory address. *x "dereferences" x and returns the data at it's address
