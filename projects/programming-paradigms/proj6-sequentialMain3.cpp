1f5db9426668742e56ccdb26bacbc57e76ef43c8
firesquid6 <jdeiss06@gmail.com>
2025-03-29 11:39:30 -0500

    add the more things

--- projects/programming-paradigms/proj6-sequentialMain3.cpp
+++ projects/programming-paradigms/proj6-sequentialMain3.cpp
@@ -1,28 +1,22 @@
-#include <string>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-const string inFile = "iperson.txt";
-const string outFile = "operson.txt";
-
 int main() {
-  string name;
+  string name, choice;
   int age;
-
-  string choice = "yes";
-  bool usingFile = false;
+  bool fileRead = false;
   ifstream pfile;
   ofstream opfile;
 
-  cout << "Would you like to read data from a file? " << endl;
+  cout << "Would you like to read the data from a file? " << endl;
   cin >> choice;
   cin.ignore(3, '\n');
 
-  usingFile = choice == "yes";
-
-  if (usingFile) {
+  if (choice == "yes") {
+    fileRead = true;
     pfile.open("iperson.txt");
     if (!pfile) {
       cerr << "Can't open person.txt for read." << endl;
@@ -33,27 +27,50 @@ int main() {
   opfile.open("operson.txt");
 
   choice = "yes";
+  if (fileRead) {
+    // readPerson equivalent inline code
+    getline(pfile, name);
+    pfile >> age;
+    pfile.ignore(3, '\n');
+    if (!pfile) {
+      choice = "no";
+    }
+  }
 
-  while( choice != "no" ) {
-    if (!usingFile) {
+  while (choice != "no") {
+    if (!fileRead) {
+      // requestInfo equivalent inline code
+      cout << "Please enter your name followed by your age." << endl;
+      
+      // readPerson equivalent inline code
+      getline(cin, name);
+      cin >> age;
+      cin.ignore(3, '\n');
+    }
+
+    // writePerson equivalent inline code
+    opfile << name << endl;
+    opfile << age << endl;
+
+    if (!fileRead) {
+      // readAgain equivalent inline code
       cout << "Do you want to read in another person (yes/no)? " << endl;
       cin >> choice;
       cin.ignore(3, '\n');
     } else {
+      // readPerson equivalent inline code
       getline(pfile, name);
       pfile >> age;
       pfile.ignore(3, '\n');
-
       if (!pfile) {
         choice = "no";
       }
     }
   }
 
-  if (usingFile) {
+  if (fileRead) {
     pfile.close();
   }
-
   opfile.close();
 
   return 0;
