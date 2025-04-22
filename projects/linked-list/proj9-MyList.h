3d4548e0c1fafbbc5c35de638f60a2c3063e651e
firesquid6 <jdeiss06@gmail.com>
2025-04-21 21:40:10 -0500

    finish the linked list assignment

--- projects/linked-list/proj9-MyList.h
+++ projects/linked-list/proj9-MyList.h
@@ -1,3 +1,16 @@
+/**
+ * file: proj9-MyList.h
+ * author: Jonathan Deiss
+ * course: CSI 1440
+ * assignment: Project 9
+ * due date: 04/21/2025
+ *
+ * date modified: 04/21/2025
+ * - file created
+ *
+ * This file contains the MyList class which implements a linked list
+ */
+
 #ifndef MY_LIST_H
 #define MY_LIST_H
 
@@ -6,71 +19,280 @@
 
 template <class T> class MyList : public ContainerIfc<T> {
 public:
+  /**
+   * MyList
+   *
+   * Default constructor that creates an empty list.
+   *
+   * Parameters:
+   * none
+   *
+   * Output:
+   * return: none
+   * reference parameters: none
+   * stream: none
+   */
   MyList();
+
+  /**
+   * ~MyList
+   *
+   * Destructor that frees all dynamically allocated memory for the list.
+   *
+   * Parameters:
+   * none
+   *
+   * Output:
+   * return: none
+   * reference parameters: none
+   * stream: none
+   */
   ~MyList();
+
+  /**
+   * MyList
+   *
+   * Copy constructor that creates a deep copy of the given MyList object.
+   *
+   * Parameters:
+   * other: the MyList object to copy from
+   *
+   * Output:
+   * return: none
+   * reference parameters: none
+   * stream: none
+   */
   MyList(const MyList &);
+
+  /**
+   * operator=
+   *
+   * Assignment operator that creates a deep copy of the given MyList object.
+   *
+   * Parameters:
+   * other: the MyList object to copy from
+   *
+   * Output:
+   * return: reference to this MyList object
+   * reference parameters: none
+   * stream: none
+   */
   MyList<T> &operator=(const MyList &);
-  MyList<T> &pushFront(T);
-  MyList<T> &pushBack(T);
-  MyList<T> &popFront(T &);
-  MyList<T> &popBack(T &);
+
+  /**
+   * pushFront
+   *
+   * Adds an element to the front of the list.
+   *
+   * Parameters:
+   * item: the element to be added to the front of the list
+   *
+   * Output:
+   * return: reference to the container interface
+   * reference parameters: none
+   * stream: none
+   */
+  ContainerIfc<T> &pushFront(T);
+
+  /**
+   * pushBack
+   *
+   * Adds an element to the back of the list.
+   *
+   * Parameters:
+   * item: the element to be added to the back of the list
+   *
+   * Output:
+   * return: reference to the container interface
+   * reference parameters: none
+   * stream: none
+   */
+  ContainerIfc<T> &pushBack(T);
+
+  /**
+   * popFront
+   *
+   * Removes and returns the element from the front of the list.
+   *
+   * Parameters:
+   * item: reference to store the removed element
+   *
+   * Output:
+   * return: reference to the container interface
+   * reference parameters: item is populated with the removed front element
+   * stream: none
+   */
+  ContainerIfc<T> &popFront(T &);
+
+  /**
+   * popBack
+   *
+   * Removes and returns the element from the back of the list.
+   *
+   * Parameters:
+   * item: reference to store the removed element
+   *
+   * Output:
+   * return: reference to the container interface
+   * reference parameters: item is populated with the removed back element
+   * stream: none
+   */
+  ContainerIfc<T> &popBack(T &);
+
+  /**
+   * getSize
+   *
+   * Returns the current number of elements in the list.
+   *
+   * Parameters:
+   * none
+   *
+   * Output:
+   * return: number of elements in the list
+   * reference parameters: none
+   * stream: none
+   */
   int getSize();
+
+  /**
+   * isEmpty
+   *
+   * Checks if the list is empty.
+   *
+   * Parameters:
+   * none
+   *
+   * Output:
+   * return: true if the list contains no elements, false otherwise
+   * reference parameters: none
+   * stream: none
+   */
   bool isEmpty();
+
+  /**
+   * front
+   *
+   * Returns the element at the front of the list.
+   *
+   * Parameters:
+   * none
+   *
+   * Output:
+   * return: the first element in the list
+   * reference parameters: none
+   * stream: none
+   */
   T front();
+
+  /**
+   * back
+   *
+   * Returns the element at the back of the list.
+   *
+   * Parameters:
+   * none
+   *
+   * Output:
+   * return: the last element in the list
+   * reference parameters: none
+   * stream: none
+   */
   T back();
+
+  /**
+   * operator[]
+   *
+   * Provides access to an element at a specific index in the list.
+   *
+   * Parameters:
+   * index: the position of the element to access
+   *
+   * Output:
+   * return: reference to the element at the specified position
+   * reference parameters: none
+   * stream: none
+   */
   T &operator[](int);
+
+  /**
+   * erase
+   *
+   * Removes all elements from the list, freeing all dynamically allocated
+   * memory.
+   *
+   * Parameters:
+   * none
+   *
+   * Output:
+   * return: none
+   * reference parameters: none
+   * stream: none
+   */
   void erase();
 
 protected:
+  /**
+   * head
+   *
+   * Pointer to the first node in the list.
+   */
   Node<T> *head;
-  
+
 private:
-  // I can't remember if you said whether you wanted
-  // this as a private method or not. If I was supposed
-  // to do it as just a normal function please pretend
-  // I did that beccause it's effectively the same thing
+  /**
+   * eraseHelper
+   *
+   * Recursive helper function to deallocate nodes from the list.
+   *
+   * Parameters:
+   * node: pointer to the current node to be deleted
+   *
+   * Output:
+   * return: none
+   * reference parameters: none
+   * stream: none
+   */
+  // couldn't remember whether this was supposed to be
+  // a private method or outside of the class. Easy to
+  // do either way, so just pretend I did whichever one
+  // you wanted me to if I did the wrong thing.
+  //
+  // felt like it made more sense to have this as a
+  // private method since its explicitly for the MyList
+  // class but honestly it could go either way
   void eraseHelper(Node<T> *);
 };
 
-template <typename T> MyList<T>::MyList() { 
-  head = NULL; 
-}
+template <typename T> MyList<T>::MyList() { head = NULL; }
 
 template <typename T> MyList<T>::~MyList() {
   Node<T> *current = head;
   Node<T> *next;
-  
+
   while (current != NULL) {
     next = current->next;
     delete current;
     current = next;
   }
-  
+
   head = NULL;
 }
 
 template <typename T> MyList<T>::MyList(const MyList<T> &other) {
   head = NULL;
-  
+
   if (other.head == NULL) {
     return;
   }
-  
+
   Node<T> *otherCurrent = other.head;
-  Node<T> *newNode = new Node<T>;
-  newNode->data = otherCurrent->data;
-  newNode->next = NULL;
-  head = newNode;
-  
+  head = new Node<T>(otherCurrent->data);
+
   Node<T> *current = head;
   otherCurrent = otherCurrent->next;
-  
+
   while (otherCurrent != NULL) {
-    newNode = new Node<T>;
-    newNode->data = otherCurrent->data;
-    newNode->next = NULL;
-    current->next = newNode;
-    
+    current->next = new Node<T>(otherCurrent->data);
     current = current->next;
     otherCurrent = otherCurrent->next;
   }
@@ -80,121 +302,109 @@ template <typename T> MyList<T> &MyList<T>::operator=(const MyList &other) {
   if (this == &other) {
     return *this;
   }
-  
+
   erase();
-  
+
   if (other.head == NULL) {
     return *this;
   }
-  
+
   Node<T> *otherCurrent = other.head;
-  Node<T> *newNode = new Node<T>;
-  newNode->data = otherCurrent->data;
-  newNode->next = NULL;
-  head = newNode;
-  
+  head = new Node<T>(otherCurrent->data);
+
   Node<T> *current = head;
   otherCurrent = otherCurrent->next;
-  
+
   while (otherCurrent != NULL) {
-    newNode = new Node<T>;
-    newNode->data = otherCurrent->data;
-    newNode->next = NULL;
-    current->next = newNode;
-    
+    current->next = new Node<T>(otherCurrent->data);
     current = current->next;
     otherCurrent = otherCurrent->next;
   }
-  
+
   return *this;
 }
 
-template <typename T> MyList<T> &MyList<T>::pushFront(T value) {
-  Node<T> *newNode = new Node<T>;
-  newNode->data = value;
+template <typename T> ContainerIfc<T> &MyList<T>::pushFront(T value) {
+  Node<T> *newNode = new Node<T>(value);
   newNode->next = head;
   head = newNode;
-  
+
   return *this;
 }
 
-template <typename T> MyList<T> &MyList<T>::pushBack(T value) {
-  Node<T> *newNode = new Node<T>;
-  newNode->data = value;
-  newNode->next = NULL;
-  
+template <typename T> ContainerIfc<T> &MyList<T>::pushBack(T value) {
+  Node<T> *newNode = new Node<T>(value);
+
   if (head == NULL) {
     head = newNode;
     return *this;
   }
-  
+
   Node<T> *current = head;
   while (current->next != NULL) {
     current = current->next;
   }
-  
+
   current->next = newNode;
   return *this;
 }
 
-template <typename T> MyList<T> &MyList<T>::popFront(T &value) {
+template <typename T> ContainerIfc<T> &MyList<T>::popFront(T &value) {
   if (head == NULL) {
     throw BADINDEX();
   }
-  
+
   value = head->data;
   Node<T> *temp = head;
   head = head->next;
   delete temp;
-  
+
   return *this;
 }
 
-template <typename T> MyList<T> &MyList<T>::popBack(T &value) {
+template <typename T> ContainerIfc<T> &MyList<T>::popBack(T &value) {
   if (head == NULL) {
     throw BADINDEX();
   }
-  
+
   if (head->next == NULL) {
     value = head->data;
     delete head;
     head = NULL;
     return *this;
   }
-  
+
   Node<T> *current = head;
   while (current->next->next != NULL) {
     current = current->next;
   }
-  
+
   value = current->next->data;
   delete current->next;
   current->next = NULL;
-  
+
   return *this;
 }
 
 template <typename T> int MyList<T>::getSize() {
   int count = 0;
   Node<T> *current = head;
-  
+
   while (current != NULL) {
     count++;
     current = current->next;
   }
-  
+
   return count;
 }
 
-template <typename T> bool MyList<T>::isEmpty() {
-  return head == NULL;
-}
+template <typename T> bool MyList<T>::isEmpty() { return head == NULL; }
 
 template <typename T> T MyList<T>::front() {
   if (head == NULL) {
     throw BADINDEX();
   }
-  
+
   return head->data;
 }
 
@@ -202,12 +412,12 @@ template <typename T> T MyList<T>::back() {
   if (head == NULL) {
     throw BADINDEX();
   }
-  
+
   Node<T> *current = head;
   while (current->next != NULL) {
     current = current->next;
   }
-  
+
   return current->data;
 }
 
@@ -215,19 +425,19 @@ template <typename T> T &MyList<T>::operator[](int index) {
   if (head == NULL || index < 0) {
     throw BADINDEX();
   }
-  
+
   Node<T> *current = head;
   int count = 0;
-  
+
   while (current != NULL && count < index) {
     current = current->next;
     count++;
   }
-  
+
   if (current == NULL) {
     throw BADINDEX();
   }
-  
+
   return current->data;
 }
 
@@ -235,7 +445,7 @@ template <typename T> void MyList<T>::eraseHelper(Node<T> *node) {
   if (node == NULL) {
     return;
   }
-  
+
   eraseHelper(node->next);
   delete node;
 }
--- projects/linked-list/proj9-TestMain.cpp
+++ projects/linked-list/proj9-TestMain.cpp
@@ -1,116 +1,96 @@
 /**
- * file: proj9-TestMain.cpp
+ * file: proj9-testMain.cpp
  * author: Jonathan Deiss
  * course: CSI 1440
  * assignment: Project 9
- * due date: 04/16/2025
+ * due date: 04/21/2025
  *
- * date modified: 04/16/2025
+ * date modified: 04/21/2025
  * - file created
  *
  * This file contains tests for the MyList template class
  */
 
-#include <iostream>
-#include <cassert>
 #include "proj9-MyList.h"
+#include <cassert>
+#include <iostream>
 
 using namespace std;
 
-/**
- * testConstructor
- *
- * Tests the default constructor for MyList
- */
 void testConstructor() {
   MyList<int> list;
   assert(list.isEmpty());
   assert(list.getSize() == 0);
-  
+
   cout << "Default constructor test passed" << endl;
 }
 
-/**
- * testPushFront
- *
- * Tests the pushFront method for MyList
- */
 void testPushFront() {
   MyList<int> list;
-  
+
   list.pushFront(10);
   assert(list.getSize() == 1);
   assert(list.front() == 10);
   assert(list.back() == 10);
-  
+
   list.pushFront(20);
   assert(list.getSize() == 2);
   assert(list.front() == 20);
   assert(list.back() == 10);
-  
+
   list.pushFront(30);
   assert(list.getSize() == 3);
   assert(list.front() == 30);
   assert(list.back() == 10);
-  
+
   cout << "pushFront test passed" << endl;
 }
 
-/**
- * testPushBack
- *
- * Tests the pushBack method for MyList
- */
 void testPushBack() {
   MyList<int> list;
-  
+
   list.pushBack(10);
   assert(list.getSize() == 1);
   assert(list.front() == 10);
   assert(list.back() == 10);
-  
+
   list.pushBack(20);
   assert(list.getSize() == 2);
   assert(list.front() == 10);
   assert(list.back() == 20);
-  
+
   list.pushBack(30);
   assert(list.getSize() == 3);
   assert(list.front() == 10);
   assert(list.back() == 30);
-  
+
   cout << "pushBack test passed" << endl;
 }
 
-/**
- * testPopFront
- *
- * Tests the popFront method for MyList
- */
 void testPopFront() {
   MyList<int> list;
   int value;
-  
+
   // Test with multiple elements
   list.pushBack(10);
   list.pushBack(20);
   list.pushBack(30);
-  
+
   list.popFront(value);
   assert(value == 10);
   assert(list.getSize() == 2);
   assert(list.front() == 20);
-  
+
   list.popFront(value);
   assert(value == 20);
   assert(list.getSize() == 1);
   assert(list.front() == 30);
-  
+
   list.popFront(value);
   assert(value == 30);
   assert(list.getSize() == 0);
   assert(list.isEmpty());
-  
+
   // Test exception on empty list
   bool exceptionThrown = false;
   try {
@@ -119,39 +99,34 @@ void testPopFront() {
     exceptionThrown = true;
   }
   assert(exceptionThrown);
-  
+
   cout << "popFront test passed" << endl;
 }
 
-/**
- * testPopBack
- *
- * Tests the popBack method for MyList
- */
 void testPopBack() {
   MyList<int> list;
   int value;
-  
+
   // Test with multiple elements
   list.pushBack(10);
   list.pushBack(20);
   list.pushBack(30);
-  
+
   list.popBack(value);
   assert(value == 30);
   assert(list.getSize() == 2);
   assert(list.back() == 20);
-  
+
   list.popBack(value);
   assert(value == 20);
   assert(list.getSize() == 1);
   assert(list.back() == 10);
-  
+
   list.popBack(value);
   assert(value == 10);
   assert(list.getSize() == 0);
   assert(list.isEmpty());
-  
+
   // Test exception on empty list
   bool exceptionThrown = false;
   try {
@@ -160,58 +135,48 @@ void testPopBack() {
     exceptionThrown = true;
   }
   assert(exceptionThrown);
-  
+
   cout << "popBack test passed" << endl;
 }
 
-/**
- * testFrontBack
- *
- * Tests the front and back methods for MyList
- */
 void testFrontBack() {
   MyList<int> list;
-  
+
   // Test exceptions on empty list
   bool frontExceptionThrown = false;
   bool backExceptionThrown = false;
-  
+
   try {
     list.front();
   } catch (BADINDEX) {
     frontExceptionThrown = true;
   }
   assert(frontExceptionThrown);
-  
+
   try {
     list.back();
   } catch (BADINDEX) {
     backExceptionThrown = true;
   }
   assert(backExceptionThrown);
-  
+
   // Test with one element
   list.pushBack(10);
   assert(list.front() == 10);
   assert(list.back() == 10);
-  
+
   // Test with multiple elements
   list.pushBack(20);
   list.pushBack(30);
   assert(list.front() == 10);
   assert(list.back() == 30);
-  
+
   cout << "front and back test passed" << endl;
 }
 
-/**
- * testBracketOperator
- *
- * Tests the [] operator for MyList
- */
 void testBracketOperator() {
   MyList<int> list;
-  
+
   // Test exception on empty list
   bool exceptionThrown = false;
   try {
@@ -220,20 +185,20 @@ void testBracketOperator() {
     exceptionThrown = true;
   }
   assert(exceptionThrown);
-  
+
   // Test with multiple elements
   list.pushBack(10);
   list.pushBack(20);
   list.pushBack(30);
-  
+
   assert(list[0] == 10);
   assert(list[1] == 20);
   assert(list[2] == 30);
-  
+
   // Test modification via reference
   list[1] = 25;
   assert(list[1] == 25);
-  
+
   // Test exception on negative index
   exceptionThrown = false;
   try {
@@ -242,7 +207,7 @@ void testBracketOperator() {
     exceptionThrown = true;
   }
   assert(exceptionThrown);
-  
+
   // Test exception on out-of-bounds index
   exceptionThrown = false;
   try {
@@ -251,152 +216,95 @@ void testBracketOperator() {
     exceptionThrown = true;
   }
   assert(exceptionThrown);
-  
+
   cout << "bracket operator test passed" << endl;
 }
 
-/**
- * testErase
- *
- * Tests the erase method for MyList
- */
 void testErase() {
   MyList<int> list;
-  
+
   // Test on empty list
   list.erase();
   assert(list.isEmpty());
-  
+
   // Test with multiple elements
   list.pushBack(10);
   list.pushBack(20);
   list.pushBack(30);
-  
+
   list.erase();
   assert(list.isEmpty());
   assert(list.getSize() == 0);
-  
+
   // Make sure we can still add elements after erasing
   list.pushBack(40);
   assert(list.getSize() == 1);
   assert(list.front() == 40);
-  
+
   cout << "erase test passed" << endl;
 }
 
-/**
- * testCopyConstructor
- *
- * Tests the copy constructor for MyList
- */
 void testCopyConstructor() {
   MyList<int> original;
-  
+
   // Test with empty list
   MyList<int> emptyCopy(original);
   assert(emptyCopy.isEmpty());
-  
+
   // Test with populated list
   original.pushBack(10);
   original.pushBack(20);
   original.pushBack(30);
-  
+
   MyList<int> copy(original);
   assert(copy.getSize() == 3);
   assert(copy[0] == 10);
   assert(copy[1] == 20);
   assert(copy[2] == 30);
-  
+
   // Test independence of copies
   copy[1] = 25;
   assert(copy[1] == 25);
   assert(original[1] == 20);
-  
+
   cout << "copy constructor test passed" << endl;
 }
 
-/**
- * testAssignmentOperator
- *
- * Tests the assignment operator for MyList
- */
 void testAssignmentOperator() {
   MyList<int> list1;
   MyList<int> list2;
-  
+
   // Test assigning from empty list
   list2 = list1;
   assert(list2.isEmpty());
-  
+
   // Test assigning from populated list
   list1.pushBack(10);
   list1.pushBack(20);
   list1.pushBack(30);
-  
+
   list2 = list1;
   assert(list2.getSize() == 3);
   assert(list2[0] == 10);
   assert(list2[1] == 20);
   assert(list2[2] == 30);
-  
+
   // Test independence of lists after assignment
   list2[1] = 25;
   assert(list2[1] == 25);
   assert(list1[1] == 20);
-  
+
   // Test self-assignment
   list1 = list1;
   assert(list1.getSize() == 3);
   assert(list1[0] == 10);
   assert(list1[1] == 20);
   assert(list1[2] == 30);
-  
+
   cout << "assignment operator test passed" << endl;
 }
 
-/**
- * testChaining
- *
- * Tests method chaining for MyList
- */
-void testChaining() {
-  MyList<int> list;
-  
-  // Test chaining pushFront
-  list.pushFront(10).pushFront(20).pushFront(30);
-  assert(list.getSize() == 3);
-  assert(list[0] == 30);
-  assert(list[1] == 20);
-  assert(list[2] == 10);
-  
-  list.erase();
-  
-  // Test chaining pushBack
-  list.pushBack(10).pushBack(20).pushBack(30);
-  assert(list.getSize() == 3);
-  assert(list[0] == 10);
-  assert(list[1] == 20);
-  assert(list[2] == 30);
-  
-  // Test chaining popFront
-  int val1, val2;
-  list.popFront(val1).popFront(val2);
-  assert(val1 == 10);
-  assert(val2 == 20);
-  assert(list.getSize() == 1);
-  
-  cout << "method chaining test passed" << endl;
-}
-
-/**
- * testDestructor
- *
- * Tests the destructor for MyList
- * (This is more of a memory leak check that would be
- * verified with tools like valgrind)
- */
 void testDestructor() {
-  // Create and destroy many lists
   for (int i = 0; i < 100; i++) {
     MyList<int> list;
     for (int j = 0; j < 100; j++) {
@@ -404,16 +312,26 @@ void testDestructor() {
     }
     // list destructor will be called at end of loop
   }
-  
-  cout << "destructor test passed (check for memory leaks separately)" << endl;
+
+  cout << "destructor test passed" << endl;
 }
 
 int main() {
   cout << "Starting MyList tests..." << endl;
-  
+
   testConstructor();
   testPushFront();
   testPushBack();
   testPopFront();
   testPopBack();
-  testFrontBack(
+  testFrontBack();
+  testBracketOperator();
+  testErase();
+  testCopyConstructor();
+  testAssignmentOperator();
+  testDestructor();
+
+  cout << "All tests passed!" << endl;
+
+  return 0;
+}
