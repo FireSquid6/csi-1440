6df5a3d09efd79ee18d6cfccde825ddd6e69451e
firesquid6 <jdeiss06@gmail.com>
2025-04-30 23:52:01 -0500

    add the vector

--- projects/queues/proj10-MyVector.h
+++ projects/queues/proj10-MyVector.h
@@ -29,4 +29,92 @@ protected:
   void shiftLeft();
 };
 
+template <typename T> MyVector<T> &MyVector<T>::operator=(const MyVector &other) {
+  if (this == &other) {
+    return *this;
+  }
+
+  delete[] data;
+
+  size = other.size;
+  capacity = other.capacity;
+  data = new T[capacity];
+
+  for (int i = 0; i < size; i++) {
+    data[i] = other.data[i];
+  }
+  
+  return *this;
+}
+
+template <typename T> MyVector<T> &MyVector<T>::pushFront(T val) {
+  if (size == capacity) {
+    grow();
+  }
+  
+  shiftRight();
+  data[0] = val;
+  size++;
+  
+  return *this;
+}
+
+template <typename T> MyVector<T> &MyVector<T>::pushBack(T val) {
+  if (size == capacity) {
+    grow();
+  }
+  
+  data[size] = val;
+  size++;
+  
+  return *this;
+}
+
+template <typename T> MyVector<T> &MyVector<T>::popFront(T &val) {
+  if (size == 0) {
+    throw BADINDEX();
+  }
+  
+  val = data[0];
+  shiftLeft();
+  size--;
+  
+  return *this;
+}
+
+template <typename T> MyVector<T> &MyVector<T>::popBack(T &val) {
+  if (size == 0) {
+    throw BADINDEX();
+  }
+  
+  val = data[size - 1];
+  size--;
+  
+  return *this;
+}
+
+template <typename T> void MyVector<T>::grow() {
+  capacity *= 2;
+  T* newData = new T[capacity]; 
+  
+  for (int i = 0; i < size; i++) {
+    newData[i] = data[i];
+  }
+
+  delete[] data;
+  data = newData; // Fixed: This should be data = newData, not newData = data
+}
+
+template <typename T> void MyVector<T>::shiftRight() {
+  for (int i = size; i > 0; i--) {
+    data[i] = data[i - 1];
+  }
+}
+
+template <typename T> void MyVector<T>::shiftLeft() {
+  for (int i = 0; i < size - 1; i++) {
+    data[i] = data[i + 1];
+  }
+}
+
 #endif
